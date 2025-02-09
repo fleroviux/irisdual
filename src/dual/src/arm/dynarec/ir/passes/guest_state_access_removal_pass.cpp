731ddef271b07152dbf98ca04ec40e7500744e27
fleroviux <fleroviux@gmail.com>
2025-02-09 23:41:49 +0100

    More cleanup

--- src/dual/src/arm/dynarec/ir/passes/guest_state_access_removal_pass.cpp
+++ src/dual/src/arm/dynarec/ir/passes/guest_state_access_removal_pass.cpp
@@ -14,26 +14,50 @@ void GuestStateAccessRemovalPass::Run(Function& function) {
 }
 
 void GuestStateAccessRemovalPass::RunBasicBlock(BasicBlock& basic_block) {
-  static constexpr auto id = [](GPR gpr, Mode cpu_mode) constexpr {
+  /**
+   * Register ID layout:
+   * Bits 0 to 4: register name (R0 to R15, SPSR, CPSR)
+   * Bits 5 to 9: CPU mode (for CPSR this is zero)
+   */
+
+  static constexpr auto gpr_id = [](GPR gpr, Mode cpu_mode) constexpr {
     const int max_shared_gpr = cpu_mode == Mode::FIQ ? 7 : 12;
     if((int)gpr <= max_shared_gpr || gpr == GPR::PC || cpu_mode == Mode::User) {
-      return (int)gpr | (int)Mode::System << 4;
+      return (size_t)Mode::System << 5 | (size_t)gpr;
     }
-    return (int)gpr | (int)cpu_mode << 4;
+    return (size_t)cpu_mode << 5 | (size_t)gpr;
   };
 
-  static constexpr auto cpsr_id = 0;
+  static constexpr auto spsr_id = [](Mode cpu_mode) constexpr {
+    return (size_t)cpu_mode << 5 | 16u;
+  };
+
+  static constexpr auto cpsr_id = 17u;
 
   // Track which IR value (if any) contains the current value of each guest GPR and PSR.
-  std::array<Value::ID, 512u> current_gpr_value{};
-  std::array<Value::ID,  32u> current_psr_value{};
+  std::array<Value::ID, 1024u> current_reg_value{};
+  current_reg_value.fill(Value::invalid_id);
 
   // Track which IR instruction (if any) most recently wrote to a guest GPR or PSR.
-  std::array<Instruction*, 512u> most_recent_gpr_store{};
-  std::array<Instruction*,  32u> most_recent_psr_store{};
+  std::array<Instruction*, 1024u> most_recent_reg_store{};
 
-  current_gpr_value.fill(Value::invalid_id);
-  current_psr_value.fill(Value::invalid_id);
+  const auto HandleLdReg = [&](Instruction* instruction, size_t reg_id, Value::ID result_value) {
+    if(current_reg_value[reg_id] == Value::invalid_id) {
+      current_reg_value[reg_id] = result_value;
+    } else {
+      RewriteValueUseRefs(basic_block, result_value, current_reg_value[reg_id]);
+      RemoveInstruction(basic_block, instruction);
+    }
+  };
+
+  const auto HandleStReg = [&](Instruction* instruction, size_t reg_id, Value::ID stored_value) {
+    current_reg_value[reg_id] = stored_value;
+
+    if(most_recent_reg_store[reg_id]) {
+      RemoveInstruction(basic_block, most_recent_reg_store[reg_id]);
+    }
+    most_recent_reg_store[reg_id] = instruction;
+  };
 
   Instruction* instruction = basic_block.head;
 
@@ -43,76 +67,36 @@ void GuestStateAccessRemovalPass::RunBasicBlock(BasicBlock& basic_block) {
         const GPR gpr = instruction->GetArg(0u).AsGPR();
         const Mode cpu_mode = instruction->GetArg(1u).AsMode();
         const Value::ID result_value = instruction->GetOut(0u);
-
-        const size_t gpr_id = id(gpr, cpu_mode);
-
-        if(current_gpr_value[gpr_id] == Value::invalid_id) {
-          current_gpr_value[gpr_id] = result_value;
-        } else {
-          RewriteValueUseRefs(basic_block, result_value, current_gpr_value[gpr_id]);
-          RemoveInstruction(basic_block, instruction);
-        }
+        HandleLdReg(instruction, gpr_id(gpr, cpu_mode), result_value);
         break;
       }
       case Instruction::Type::STGPR: {
         const GPR gpr = instruction->GetArg(0u).AsGPR();
         const Mode cpu_mode = instruction->GetArg(1u).AsMode();
         const Value::ID stored_value = instruction->GetArg(2u).AsValue();
-
-        const size_t gpr_id = id(gpr, cpu_mode);
-
-        current_gpr_value[gpr_id] = stored_value;
-
-        if(most_recent_gpr_store[gpr_id]) {
-          RemoveInstruction(basic_block, most_recent_gpr_store[gpr_id]);
-        }
-        most_recent_gpr_store[gpr_id] = instruction;
+        HandleStReg(instruction, gpr_id(gpr, cpu_mode), stored_value);
         break;
       }
       case Instruction::Type::LDCPSR: {
         const Value::ID result_value = instruction->GetOut(0u);
-
-        if(current_psr_value[cpsr_id] == Value::invalid_id) {
-          current_psr_value[cpsr_id] = result_value;
-        } else {
-          RewriteValueUseRefs(basic_block, result_value, current_psr_value[cpsr_id]);
-          RemoveInstruction(basic_block, instruction);
-        }
+        HandleLdReg(instruction, cpsr_id, result_value);
         break;
       }
       case Instruction::Type::STCPSR: {
         const Value::ID stored_value = instruction->GetArg(0u).AsValue();
-
-        current_psr_value[cpsr_id] = stored_value;
-
-        if(most_recent_psr_store[cpsr_id]) {
-          RemoveInstruction(basic_block, most_recent_psr_store[cpsr_id]);
-        }
-        most_recent_psr_store[cpsr_id] = instruction;
+        HandleStReg(instruction, cpsr_id, stored_value);
         break;
       }
       case Instruction::Type::LDSPSR: {
         const Mode cpu_mode = instruction->GetArg(0u).AsMode();
         const Value::ID result_value = instruction->GetOut(0u);
-
-        if(current_psr_value[(int)cpu_mode] == Value::invalid_id) {
-          current_psr_value[(int)cpu_mode] = result_value;
-        } else {
-          RewriteValueUseRefs(basic_block, result_value, current_psr_value[(int)cpu_mode]);
-          RemoveInstruction(basic_block, instruction);
-        }
+        HandleLdReg(instruction, spsr_id(cpu_mode), result_value);
         break;
       }
       case Instruction::Type::STSPSR: {
         const Mode cpu_mode = instruction->GetArg(0u).AsMode();
         const Value::ID stored_value = instruction->GetArg(1u).AsValue();
-
-        current_psr_value[(int)cpu_mode] = stored_value;
-
-        if(most_recent_psr_store[(int)cpu_mode]) {
-          RemoveInstruction(basic_block, most_recent_psr_store[(int)cpu_mode]);
-        }
-        most_recent_psr_store[(int)cpu_mode] = instruction;
+        HandleStReg(instruction, spsr_id(cpu_mode), stored_value);
         break;
       }
       default: {
