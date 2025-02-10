a0c9cc2a125e7d79dfd44868402ca9cb50f5c20a
fleroviux <fleroviux@gmail.com>
2025-02-11 00:11:18 +0100

    Doc

--- src/dual/src/arm/dynarec/ir/passes/host_flag_propagation_pass.hpp
+++ src/dual/src/arm/dynarec/ir/passes/host_flag_propagation_pass.hpp
@@ -11,9 +11,13 @@ namespace dual::arm::jit::ir {
 /**
  * This IR pass tracks where flags are originally created and if possible rewrites instructions
  * with host flag inputs to refer to the original host flags value where the relevant flags were created.
- * This allows tracing the flags going into a conditional branch (BR_IF) back to the SUB.S instruction which created them,
- * even if the original host flags value was converted to NZCV, merged into old CPSR, written to CPSR, loaded back from
- * CPSR and then converted back to a host flags value:
+ * This makes it possible to for example trace flags going into a BR_IF instruction back to the host flags output of an
+ * SUB.S, which originally computed the flags.
+ *
+ * This works even if the original host values value got converted to a NZCV value,
+ * merged with another NZCV value (usually previous CPSR value) and then converted back to a host flags value.
+ *
+ * In the following IR code:
  * ```
  * v2, v3 := sub.s v0, v1
  * v4     := ldcpsr
@@ -21,12 +25,16 @@ namespace dual::arm::jit::ir {
  * v6     := ldconst 0xF0000000
  * v7     := bitcmb v4, v5, v6
  *           stcpsr v7
- * v8     := ldcpsr
- * v9     := cvt.nzcv.hflag v8
- *           br_if eq, v9, @bb_1, @bb_2
+ * v8     := cvt.nzcv.hflag v7
+ *           br_if eq, v8, @bb_1, @bb_2
  * ```
+ * the BR_IF instruction will be rewritten to use v3 instead of v8.
+ * The CVT.NZCV.HFLAG instruction can later be removed by dead code removal.
  *
- * The pass requires Guest State Access Removal Pass to run first in order to function properly.
+ * This pass helps not only with removing unnecessary host flags to NZCV conversions, but also with avoiding
+ * unnecessary loads of host flag values into the host flags register when the original host flags value still is in that register.
+ *
+ * The pass requires Guest State Access Removal Pass to run first in order to function as expected.
  */
 
 class HostFlagPropagationPass final : public Pass {
