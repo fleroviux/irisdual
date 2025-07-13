9e6597b05d4f155d4c07adf1b31404f3cb36bf37
fleroviux <fleroviux@gmail.com>
2025-07-14 00:52:44 +0200

    platform: sdl: Disallow audio buffer size change, since it behaves weirdly on Windows

--- src/platform/sdl/src/sdl2_audio_driver.cpp
+++ src/platform/sdl/src/sdl2_audio_driver.cpp
@@ -21,22 +21,24 @@ bool SDL2AudioDriver::Open(uint sample_rate, uint buffer_size) {
   want.freq = (int)sample_rate;
   want.samples = buffer_size;
   want.format = AUDIO_S16;
-  want.channels = 2;
+  want.channels = k_channel_count;
   want.callback = (SDL_AudioCallback)SDL2AudioDriver_AudioCallback;
   want.userdata = this;
 
-  m_audio_device = SDL_OpenAudioDevice(nullptr, 0, &want, &m_have, SDL_AUDIO_ALLOW_SAMPLES_CHANGE);
+  m_audio_device = SDL_OpenAudioDevice(nullptr, 0, &want, &m_have, 0);
 
   if(m_audio_device == 0) {
     ATOM_ERROR("SDL_OpenAudioDevice: failed to open audio: %s\n", SDL_GetError());
     return false;
   }
 
+  // TODO: can this ever trigger?
   if(m_have.format != want.format) {
     ATOM_ERROR("SDL_AudioDevice: S16 sample format unavailable.");
     return false;
   }
 
+  // TODO: can this ever trigger?
   if(m_have.channels != want.channels) {
     ATOM_ERROR("SDL_AudioDevice: Stereo output unavailable.");
     return false;
@@ -57,7 +59,6 @@ void SDL2AudioDriver::Close() {
 }
 
 uint SDL2AudioDriver::GetBufferSize() const {
-  // TODO: this is confusing to the API user because the audio buffer size is twice of SDL's buffer size.
   return m_audio_buffer.size() / k_channel_count;
 }
 
